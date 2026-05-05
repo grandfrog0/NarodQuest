d40d4232bdadbf0fe4a43d887e6dcbf954218249
konopaty11 <kkonopaty@gmail.com>
2026-05-05 10:00:47 +0500

    Finished player movement controller and camera controller

---
+++ Assets/Scripts/Configs/PlayerConfig.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerConfig", menuName = "Scriptable Objects/PlayerConfig")]
+public class PlayerConfig : ScriptableObject
+{
+    public float speed;
+}
--- Assets/Scripts/Movement/JoystickController.cs
+++ Assets/Scripts/Movement/JoystickController.cs
@@ -1,9 +1,13 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class JoystickController : MonoBehaviour, IDragHandler, IEndDragHandler
 {
+    [SerializeField] bool xInverse;
     [SerializeField] float maxDistance;
+
+    public Vector2 Movement { get; private set; }
 
     RectTransform _joystick;
     Vector2 _startPosition;
@@ -23,15 +27,26 @@ public class JoystickController : MonoBehaviour, IDragHandler, IEndDragHandler
     {
         Vector2 targetPosition = eventData.position;
 
-        Vector2 direction = _startPosition - targetPosition;
+        // инверсия в зависимости от якорей джостика
+        targetPosition.x -= xInverse ? Screen.width : 0f;
+
+        Vector2 direction = targetPosition - _startPosition;
         direction.Normalize();
+        // уменьшение магнитуды для реализации разгона
+        direction = Vector2.ClampMagnitude(direction, 1 / maxDistance);
+
+        Debug.Log(direction);
 
         float distance = Vector3.Distance(_startPosition, targetPosition);
         float clampedDistance = Mathf.Clamp(distance, 0f, maxDistance);
+        
+        Movement = direction * clampedDistance;
+        _joystick.anchoredPosition = _startPosition + Movement * clampedDistance;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        
+        _joystick.anchoredPosition = _startPosition;
+        Movement = Vector2.zero;
     }
 }
